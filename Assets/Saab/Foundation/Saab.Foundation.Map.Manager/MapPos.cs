e79ab9dd8b22d9489cb5d1174197e97291f6fe3a
Anders Modén <anders.moden@saabgroup.com>
2022-03-03 08:19:50 +0100

    work on gizmo stream

--- Assets/Saab/Foundation/Saab.Foundation.Map.Manager/MapPos.cs
+++ Assets/Saab/Foundation/Saab.Foundation.Map.Manager/MapPos.cs
@@ -19,7 +19,7 @@
 // Module		: Saab.Foundation.Map.Manager
 // Description	: Definition of the MapPos map position structure
 // Author		: Anders Modén		
-// Product		: Gizmo3D 2.11.67
+// Product		: Gizmo3D 2.11.70
 //
 // NOTE:	Gizmo3D is a high performance 3D Scene Graph and effect visualisation 
 //			C++ toolkit for Linux, Mac OS X, Windows, Android, iOS and HoloLens for  
